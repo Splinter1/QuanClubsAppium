38d1576c1de9c0b53350eafe7e104bc249e4de92
Splinter1 <574222018@qq.com>
2019-03-22 15:33:03 +0800

    登陆页面已完成

--- src/main/java/actions/AllowPermission.java
+++ src/main/java/actions/AllowPermission.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.By;
 
 public class AllowPermission {
 
-    public static void allowPer(){
+    public static void allowPer()throws  Exception{
         Actions.click(By.id("com.android.packageinstaller:id/permission_allow_button"));
         Actions.click(By.id("com.android.packageinstaller:id/permission_allow_button"));
         Actions.click(By.id("com.android.packageinstaller:id/permission_allow_button"));
         //跳过广告
+        Thread.sleep(2000);
         Actions.click(By.id("com.mingtimes.quanclubs:id/skip_view"));
     }
 }
--- src/main/quanclubs/loginpage/RegisterPage.java
+++ src/main/quanclubs/loginpage/RegisterPage.java
@@ -17,7 +17,6 @@ public class RegisterPage {
 
     private AndroidDriver driver;
     private  String phone = RandomNum.randomPhone();
-    private  String passwd = "123456";
 
     @BeforeClass
     public void setup()throws Exception{
@@ -28,11 +27,11 @@ public class RegisterPage {
     @Test
     public void aExistingPhone() throws Exception {
         //点击注册按钮
-        Actions.click(By.id("com.mingtimes.loginpage:id/goRegister"));
+        Actions.click(By.id("com.mingtimes.quanclubs:id/goRegister"));
         //输入已存在的手机号
-        Actions.sendKeys(By.id("com.mingtimes.loginpage:id/userMobile") , "15312200089");
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userMobile") , "15312200089");
         //点击发送验证码
-        Actions.click(By.id("com.mingtimes.loginpage:id/vaildCode"));
+        Actions.click(By.id("com.mingtimes.quanclubs:id/vaildCode"));
         //校验是否成功
         try{
             getText(By.xpath("//*[contains(@text,'手机号码已存在')]"));
@@ -44,12 +43,16 @@ public class RegisterPage {
     @Test
     public void bRegister()throws Exception{
         //输入正确手机号
-        Actions.sendKeys(By.id("com.mingtimes.loginpage:id/userMobile") , phone);
-        Actions.sendKeys(By.id("com.mingtimes.loginpage:id/userPazz") , passwd);
-        Actions.sendKeys(By.id("com.mingtimes.loginpage:id/userPazzRepeat") , passwd);
-        Actions.sendKeys(By.id("com.mingtimes.loginpage:id/userVCode") , "000000");
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userMobile") , phone);
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userPazz") , "000000");
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userPazzRepeat") , "000000");
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userVCode") , "000000");
         //点击发送验证码
-        Actions.click(By.id("com.mingtimes.loginpage:id/vaildCode"));
+        Actions.click(By.id("com.mingtimes.quanclubs:id/vaildCode"));
+        //同意协议
+        Actions.click(By.id("com.mingtimes.quanclubs:id/radioBtn"));
+        //点击注册
+        Actions.click(By.id("com.mingtimes.quanclubs:id/btnRegister"));
         //校验是否成功
         try{
             String a = Actions.getText(By.id("com.mingtimes.quanclubs:id/btnLogin"));
@@ -60,11 +63,55 @@ public class RegisterPage {
     }
 
     @Test
-    public void cLogin(){
+    public void cChangePasswd()throws Exception{
+        //进入忘记密码页面
+        Actions.click(By.id("com.mingtimes.quanclubs:id/forgetPazz"));
+        //判断是否为找回密码页面
+        String a = Actions.getText(By.id("com.mingtimes.quanclubs:id/userForget"));
+        Assert.assertEquals(a,"找回密码");
+        //输入不存在的手机号码
+        String newphone = RandomNum.randomPhone();
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userMobile") , newphone);
+        Actions.click(By.id("com.mingtimes.quanclubs:id/vaildCode"));
+        try{
+            getText(By.xpath("//*[contains(@text,'手机号码不存在')]"));
+        }catch (Exception e){
+            throw new Exception("报错：手机号码不存在");
+        }
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userMobile") , phone);
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userPazz") , "123456");
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userPazzRepeat") , "123456");
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userVCode") , "000000");
+        //点击发送验证码
+        Actions.click(By.id("com.mingtimes.quanclubs:id/vaildCode"));
+        //点击确认
+        Actions.click(By.id("com.mingtimes.quanclubs:id/btnSure"));
+        //校验是否成功
+        try{
+            String b = Actions.getText(By.id("com.mingtimes.quanclubs:id/btnLogin"));
+            Assert.assertEquals(b,"立即登录");
+        }catch (Exception e){
+            throw new Exception("注册失败");
+        }
+    }
+
+
+
+    @Test
+    public void dLogin()throws Exception{
         //输入错误密码
-        Actions.sendKeys(By.id("com.mingtimes.loginpage:id/userMobile") , phone);
-        Actions.sendKeys(By.id("com.mingtimes.loginpage:id/userPazz") , "000000");
-        Actions.click(By.id("com.mingtimes.loginpage:id/btnLogin"));
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userMobile") , phone);
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userPazz") , "000000");
+        Actions.click(By.id("com.mingtimes.quanclubs:id/btnLogin"));
+        try{
+            Actions.getText(By.xpath("//*[contains(@text,'账号不存在或密码错误')]"));
+        }catch (Exception e){
+            throw new Exception("断言失败");
+        }
+        //输入正确账号密码
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userMobile") , phone);
+        Actions.sendKeys(By.id("com.mingtimes.quanclubs:id/userPazz") , "123456");
+        Actions.click(By.id("com.mingtimes.quanclubs:id/btnLogin"));
     }
 
     @AfterClass
